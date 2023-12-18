8f5d835bbe8defd3d27719f6e5d5d6610618a67d
Nikola Popovic <126015569+popislavv@users.noreply.github.com>
2023-12-18 12:06:24 +0100

    Add files via upload

---
+++ frontend/js/birdColors.js
@@ -0,0 +1,38 @@
+const birdColors = [
+    {
+        color: 'Yellow',
+        price: 0
+    },
+    {
+        color: 'boki',
+        price: 50
+    },
+    {
+        color: 'buza',
+        price: 60
+    },
+    {
+        color: 'tesa',
+        price: 80
+    },
+    {
+        color: 'pop',
+        price: 80
+    }, 
+    {
+        color: 'dani',
+        price: 100
+    },
+    {
+        color: 'pero',
+        price: 250
+    },
+    {
+        color: 'luka',
+        price: 300
+    },
+    {
+        color: 'risa',
+        price: 800
+    },
+];
